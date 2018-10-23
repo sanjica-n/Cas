5705c1af16b15cce8502d6cd59f8386851b1afdf
Dell06 <Dell06@DESKTOP-CF9H2IP>
2018-10-23 10:11:16 +0200

    Datum

--- Kalendar/src/Datum.java
+++ Kalendar/src/Datum.java
@@ -25,6 +25,18 @@ public class Datum {
 		   this.godina = 2010;
 		   }*/
 	   public Datum() {
-		   this(01, 01, 2010)
+		   this(01, 01, 2010);
 	   }
+	   public Boolean proveriDatum() {
+		   //ako je datum isravan vraca true
+		   //ako nije ispravan vraca false
+		   if(mesec >= 1 && mesec <= 12 && dan >= 1 && dan <=31) {
+			   System.out.println("datum je u zadatom opsegu");
+			   return true;
+		   }
+		   System.out.println("datum nije u zadatom opsegu!");
+		   return false;
+	   }
+	   
+	   
 }
--- Kalendar/src/Demorad.java
+++ Kalendar/src/Demorad.java
@@ -4,7 +4,15 @@ public class Demorad {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        Datum d =new Datum();
+       d.proveriDatum();
        System.out.println(d.datum());
+       
+       Datum d2 = new Datum(23,10,2018);
+       System.out.println(d2.datum());
+       System.out.println(d2.proveriDatum());
+       
+       
 	}
 
 }
+  
